d2a3ee1818c4a562ef75eea0e89e56e1074937fa
Hatem Ben Hamza <hatembenhamza96@gmail.com>
2019-03-29 12:45:44 +0100

    second commit

---
+++ src/EcommerceBundle/Entity/Produit.php~
@@ -0,0 +1,206 @@
+<?php
+
+namespace EcommerceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Produit
+ *
+ * @ORM\Table(name="produit")
+ * @ORM\Entity(repositoryClass="EcommerceBundle\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_date", type="datetime")
+     */
+    private $createdDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Produit
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Produit
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return Produit
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set createdDt
+     *
+     * @param \DateTime $createdDt
+     *
+     * @return Produit
+     */
+    public function setCreatedDt($createdDt)
+    {
+        $this->createdDt = $createdDt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdDt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDt()
+    {
+        return $this->createdDt;
+    }
+
+    /**
+     * Set createdDate
+     *
+     * @param \DateTime $createdDate
+     *
+     * @return Produit
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createdDate
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param integer $stock
+     *
+     * @return Produit
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return integer
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+}
--- src/TaskBundle/Controller/DefaultController.php
+++ src/TaskBundle/Controller/DefaultController.php
@@ -3,6 +3,16 @@
 namespace TaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TaskBundle\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
 
 class DefaultController extends Controller
 {
@@ -10,6 +20,7 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
         $tasks = $em->getRepository('TaskBundle:Task')->findAll();
+        
         return $this->render('TaskBundle:Default:index.html.twig',array(
           'tasks' => $tasks
         ));
@@ -24,4 +35,76 @@ class DefaultController extends Controller
 
         return $this->redirect($this->generateUrl('task_homepage'));
     }
+
+    public function addAction(Request $request){
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $task = new Task();
+
+        $form = $this->createFormBuilder($task)
+        ->add('name',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('description',TextareaType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('due_date',DateTimeType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('priority',ChoiceType::class,array('choices'=>array('Low'=>'Low','Medium'=>'Medium','High'=>'High'),'attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('submit',SubmitType::class,array('attr'=>array('class'=>'col-md-4 form-control btn btn-primary')))
+        ->getForm();
+
+         $form->handleRequest($request);
+         
+         if($form->isSubmitted()){
+            $task->setName($form['name']->getData());
+            $task->setDescription($form['description']->getData());
+            $task->setPriority($form['priority']->getData());
+            $task->setCreatedDate(new \DateTime('now'));
+            $task->setDueDate($form['due_date']->getData());
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success','your task has been successfuly persisted !');
+
+            return $this->redirect($this->generateUrl('task_homepage'));
+         }
+
+        return $this->render('TaskBundle:Default:create_task.html.twig',array(
+            'form' => $form->createView()
+        ));
+
+    }
+
+    public function updateAction(Request $request,$id){
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $task = $em->getRepository('TaskBundle:Task')->find($id);
+
+        $form = $this->createFormBuilder($task)
+        ->add('name',TextType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('description',TextareaType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('due_date',DateTimeType::class,array('attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('priority',ChoiceType::class,array('choices'=>array('Low'=>'Low','Medium'=>'Medium','High'=>'High'),'attr'=>array('class'=>'col-md-4 form-control')))
+        ->add('submit',SubmitType::class,array('attr'=>array('class'=>'col-md-4 form-control btn btn-primary')))
+        ->getForm();
+
+         $form->handleRequest($request);
+         
+         if($form->isSubmitted()){
+            $task->setName($form['name']->getData());
+            $task->setDescription($form['description']->getData());
+            $task->setPriority($form['priority']->getData());
+            $task->setCreatedDate(new \DateTime('now'));
+            $task->setDueDate($form['due_date']->getData());
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success','your task has been successfuly updated !');
+
+            return $this->redirect($this->generateUrl('task_homepage'));
+         }
+
+        return $this->render('TaskBundle:Default:task_update.html.twig',array(
+            'form' => $form->createView()
+        ));
+    }
+
 }
